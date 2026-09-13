270ebc64f3fa7aa9ce94e19e8a9f445b99eac0bb
nayeem <vvnayeem1@gmail.com>
2026-09-14 02:08:14 +0400

    fix(backend): add EC2 IP to sanctum stateful domains

--- config/sanctum.php
+++ config/sanctum.php
@@ -20,7 +20,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,3.111.32.57,',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
